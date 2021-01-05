142aecae289d165a664e2bbcb38eda7ae46017d2
versae19 <versae19@gmail.com>
2021-01-05 12:08:01 +0530

    registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { RegisterComponent } from './register/register.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path:'about-us',
-    component: AboutUsComponent
-  }
+  { path: '', component: IndexComponent, pathMatch:'full' },
+  { path: 'index', component: IndexComponent },
+  { path:'about-us', component: AboutUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    //CommonModule,
     RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  register(){
+    this.router.navigate(['register'],{relativeTo:this.route})
   }
 
 }
